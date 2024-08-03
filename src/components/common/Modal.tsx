90fcab918f63cfb1101d48f4d6adb586882d5149
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2024-08-03 21:43:26 +0530

    fix_modal_error

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { createPortal } from "react-dom";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
 type ModalProps = {
@@ -9,42 +8,34 @@ type ModalProps = {
 	onClose: () => void;
 };
 
-export const Modal = ({
-	children,
-	id,
-	openModal,
-	onClose,
-}: ModalProps) => {
-	return createPortal(
-		openModal ? (
-			<>
-				<input
-					type="checkbox"
-					id={id}
-					className="modal-toggle"
-					checked={openModal}
-					readOnly
-				/>
-				<div className="modal" role="dialog">
-					<div className="modal-box relative">
-						<button
-							onClick={onClose}
-							className="absolute right-3 top-3">
-							<XMarkIcon className="size-5 stroke-[2px] text-gray-500" />
-						</button>
-						{children}
-					</div>
-					<label
-						className="modal-backdrop"
-						htmlFor={id}
-						onClick={onClose}>
-						Close
-					</label>
+const Modal = ({ children, id, openModal, onClose }: ModalProps) => {
+	return openModal ? (
+		<>
+			<input
+				type="checkbox"
+				id={id}
+				className="modal-toggle"
+				checked={openModal}
+				readOnly
+			/>
+			<div className="modal" role="dialog">
+				<div className="modal-box relative">
+					<button
+						onClick={onClose}
+						className="absolute right-3 top-3">
+						<XMarkIcon className="size-5 stroke-[2px] text-gray-500" />
+					</button>
+					{children}
 				</div>
-			</>
-		) : null,
-		document.body,
-	);
+				<label
+					className="modal-backdrop"
+					htmlFor={id}
+					onClick={onClose}>
+					Close
+				</label>
+			</div>
+		</>
+	) : null;
 };
 
 export default Modal;
